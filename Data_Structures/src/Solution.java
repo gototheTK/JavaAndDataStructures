edc057eb11de6e11bde640753a18389c55a291d4
gototheTK <gototkno1@gmail.com>
2023-04-11 15:47:09 +0900

    arrayManipulation

--- Data_Structures/src/Solution.java
+++ Data_Structures/src/Solution.java
@@ -3,29 +3,45 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        // Write your code here.
-        if (s.length() == 0) {
-            System.out.println("0");
-            return;
-        }
-        String[] strs = s.split("[\\s!,?._'@]+");
+    public static long arrayManipulation(int n, List<List<Integer>> queries) {
+        // Write your code here
 
-        if (strs[0].equals("")) {
-            System.out.println(strs.length - 1);
-        } else {
-            System.out.println(strs.length);
-        }
+        long[] numbers = new long[n];
+        TreeSet<Integer> indexes = new TreeSet<>();
+        long max = 0;
 
-        for (String str : strs) {
-            if (!str.equals("")) {
-                System.out.println(str.length());
-                System.out.println(str);
+        List<Integer> temp = queries.remove(0);
+
+        numbers[temp.get(0) - 1] = temp.get(2);
+        numbers[temp.get(1) - 1] = temp.get(2);
+
+        for (List<Integer> query : queries) {
+
+            Integer from = query.get(0) - 1;
+            Integer to = query.get(1) - 1;
+            long value = query.get(2).longValue();
+
+            Integer frontFrom = indexes.floor(from);
+            frontFrom = null == frontFrom ? from : frontFrom;
+            Integer backTo = indexes.ceiling(to);
+            backTo = null == backTo ? to : backTo;
+
+            indexes.add(from);
+            indexes.add(to);
+
+            Iterator<Integer> fromTo = indexes.subSet(frontFrom, true, backTo, false).iterator();
+
+            while (fromTo.hasNext()) {
+
+                Integer index = fromTo.next();
+                numbers[index] += value;
+                max = numbers[index] > max ? numbers[index] : max;
+
             }
 
         }
-        scan.close();
+
+        return max;
     }
+
 }
