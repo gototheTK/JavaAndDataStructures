7a945cef4bfad2dfb9c42274c275910a475ba105
gototheTK <gototkno1@gmail.com>
2023-04-14 15:48:13 +0900

    prepare/hackerRank/Algorithms

--- Data_Structures/src/prapare/hackerrank/SQL/SQL.sql
+++ Data_Structures/src/prapare/hackerrank/SQL/SQL.sql
@@ -19,3 +19,21 @@ Population Census
 
 
 SELECT SUM(CITY.POPULATION) FROM CITY INNER JOIN COUNTRY ON CITY.COUNTRYCODE = COUNTRY.CODE WHERE COUNTRY.CONTINENT = 'Asia';
+
+
+/**
+African Cities
+**/
+
+SELECT c1.name FROM city c1 INNER JOIN country c2 ON c1.countrycode = c2.code WHERE c2.continent = 'Africa';
+
+/**
+Average Population of Each Continent
+
+Submissions	Leaderboard	Discussions
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+**/
+
+SELECT C2.CONTINENT, TRUNC(AVG(C1.POPULATION)) FROM CITY C1 INNER JOIN COUNTRY C2 ON C1.COUNTRYCODE = C2.CODE GROUP BY CONTINENT;
