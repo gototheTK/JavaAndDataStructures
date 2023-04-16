d3c332cf6363017bda96032187c2cf7f9ae425b0
gototheTK <gototkno1@gmail.com>
2023-04-17 04:50:25 +0900

    problems/programmers/sql

---
+++ Data_Structures/src/problems/hackerrank/DataStructures/DayOfProgrammer.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'dayOfProgrammer' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER year as parameter.
+     */
+
+    public static String dayOfProgrammer(int year) {
+        // Write your code here
+
+        String answer = ".09." + year;
+
+        int day = 12;
+
+        if (1918 == year) {
+            return "26.09.1918";
+        }
+        if (year > 1918 && (year % 100 != 0 && year % 4 == 0 || year % 400 == 0)) {
+            day = 12;
+        } else if (year <= 1918 && year % 4 == 0) {
+            day = 12;
+        } else {
+            day = 13;
+        }
+
+        return day + answer;
+
+    }
+
+}
+
+public class DayOfProgrammer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int year = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.dayOfProgrammer(year);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
