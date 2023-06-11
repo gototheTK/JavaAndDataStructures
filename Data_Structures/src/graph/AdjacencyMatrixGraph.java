04138b415eea12a1a55b2ba3bf24e0bb4b4b15c0
gototheTK <gototkno1@gmail.com>
2023-06-11 19:31:41 +0900

    Coding_Test/Programmers/Running_race

--- Data_Structures/src/graph/AdjacencyMatrixGraph.java
+++ Data_Structures/src/graph/AdjacencyMatrixGraph.java
@@ -1,111 +1,64 @@
 package graph;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 
+import javax.sound.midi.SysexMessage;
+
+/**
+ * AdjacencyMatrix
+ */
 public class AdjacencyMatrixGraph {
 
-    // 열과 행의 인덱스는 정점을 표현하고, 배열값은 간선을 여부를 표현한다.
+    // 열 과 행의 인덱스는 정점을 표현하고, 배열값은 간선을 여부를 표현한다.
     final int[][] graph;
     final int[] visit;
 
     public AdjacencyMatrixGraph(int size) {
         graph = new int[size][size];
-        visit = new int[size];
+        visit = new int[graph.length];
     }
 
     public void addUndirectedEdge(int u, int v) {
-        // TODO: 무방향 간선
-        addUndirectedEdge(u, v, 1);
+        // TODO : 무방향 간선
+        this.addUndirectedEdge(u, v, 1);
     }
 
     public void addUndirectedEdge(int u, int v, int w) {
+        // TODO : 무방향 간선
         graph[u][v] = w;
         graph[v][u] = w;
     }
 
-    public void addDirectedEdge(int u, int v) {
-        addDirectedEdge(u, v, 1);
+    public void addDirectEdge(int u, int v) {
+        this.addDirectEdge(u, v, 1);
     }
 
-    public void addDirectedEdge(int u, int v, int w) {
+    public void addDirectEdge(int u, int v, int w) {
         graph[u][v] = w;
-        graph[v][u] = w;
     }
 
     public void clear() {
+        Arrays.fill(this.visit, 0);
         for (int i = 0; i < graph.length; i++) {
-            Arrays.fill(graph, 0);
+            Arrays.fill(graph[i], 0);
         }
     }
 
-    public void dfsTraversal(int start) {
-        // TODO : start 매개변수는 탐색 시작의 정점이다.
-        Arrays.fill(visit, 0);
-        System.out.println("---Matrix dfs---");
-        dfs(start);
-        System.out.println("-----------------");
-    }
-
-    private void dfs(int u) {
-        // TODO : 깊이 우선 탐색
-        if (1 == visit[u]) {
-            return;
-        }
-
-        visit(u);
-        for (int i = 0; i < graph[u].length; i++) {
-            if (0 != graph[u][i] && 0 == visit[u]) {
-                dfs(i);
-            }
-        }
-    }
-
-    public void bfsTraversal(int start) {
-        Arrays.fill(visit, 0);
-        System.out.println("---Matrix bfs---");
-        bfs(start);
-        System.out.println("-----------------");
-    }
-
-    private void bfs(int start) {
-
-        LinkedList<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visit(start);
-        while (!queue.isEmpty()) {
-            int vertex = queue.remove();
-            for (int i = 0; i < graph[vertex].length; i++) {
-
-                if (0 == visit[vertex] && 0 == graph[vertex][i]) {
-                    queue.add(vertex);
-                    visit(vertex);
-                }
-            }
-
-        }
-
-    }
-
-    private void visit(int vertex) {
-        System.out.println("vertex:%d" + vertex);
+    void visit(int vertex) {
+        System.out.printf("vertex:%d\n", vertex);
         visit[vertex] = 1;
     }
 
-    public void printTree() {
-
+    public void printEdgd() {
         System.out.println("---Matrix Edge---");
         for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < graph.length; j++) {
+            for (int j = 0; j < graph[i].length; j++) {
                 if (0 != graph[i][j]) {
-                    System.out.printf("(%d, %d, %d)", i, j, graph[i][j]);
+                    System.out.printf("(%d,%d,%d)", i, j, graph[i][j]);
                 }
             }
         }
-        System.out.println("\n--------------");
-
+        System.out.println("\n---------------");
     }
 
 }
---
+++ Data_Structures/src/problems/programmers/Java/Running_Race.java
@@ -0,0 +1,29 @@
+package problems.programmers.Java;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Running_Race {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = Arrays.copyOf(players, players.length);
+
+        Map<String, Integer> ranks = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++) {
+            ranks.put(players[i], i + 1);
+        }
+
+        for (String player : callings) {
+
+            Integer rank = ranks.get(player);
+            String overturned = answer[rank - 2];
+            answer[rank - 2] = answer[rank - 1];
+            answer[rank - 1] = overturned;
+            ranks.put(answer[rank - 2], rank - 1);
+            ranks.put(answer[rank - 1], rank);
+        }
+
+        return answer;
+    }
+}
